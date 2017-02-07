13ff9d30687149ffcfa8162e0fb461c4e20c3372
QuaintShanty <quaint.shanty@gmail.com>
2017-02-07 05:16:44 -0500

    Trivia questions command semi-implemented

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ const fs = require("fs");
 
 var data = {
     trivia: [
-        [
             // Themes
             {
                 name: "Numbers",
@@ -25,7 +24,6 @@ var data = {
                     { question: "What is the second letter?", answers: ["B", "b"] }    
                 ]
             },
-        ]
     ],
     temp: null
 };
@@ -53,10 +51,7 @@ bot.on("message", function(message) {
  
         // Determine if the message has params after the command
         if (message.content.includes(' ')) {
-            //params = message.content.substring(tempVal + 1, message.content.length).split(/("[^"]*"|'[^']*'|[\S]+)+/);
-            params = split_params(message.content.substring(tempVal + 1, message.content.length));
-            params = params.slice(1, params.length - 1);
-            //params = params.slice(-1, 1); // remove beginning and end until regex is changed
+            params = splitParams(message.content.substring(tempVal + 1, message.content.length));
         }
         
         switch (command) {
@@ -81,33 +76,101 @@ Answers: Answer here"`
                 console.log(params);
                 console.log("[0]" + params[0]);
                 switch(params[0]) {
+                    // Themes are general topics for the trivia. (finished)
+                    // This sub-command enables the users to do the following:
+                    // Add new themes to the data array.
+                    // Remove themes from the data array.
+                    // Show all of the themes currently present in the data array.
                     case "themes":
                         if(params.length < 2) {
-                            message.channel.sendMessage("Incorrect parameters: `!trivia themes <add|remove|show> \"<name>\"`");
+                            message.channel.sendMessage("Incorrect parameters: `!trivia themes <add|remove|show>`");
                             break;
                         }
                         
                         console.log("[1]" + params[1]);
                         switch(params[1]) {
                             case "add":
+                                if(params[2] == null) {
+                                    message.channel.sendMessage("Incorrect parameters: `!trivia themes add \"<name>\"`");
+                                }
+                                addTrivia(params[2]);
                                 break;
                             
                             case "remove":
-                                data.trivia.remove()
+                                if(params[2] == null) {
+                                    message.channel.sendMessage("Incorrect parameters: `!trivia themes remove <index>`");
+                                    break;
+                                }
+                                
+                                var themeIndex = params[2];
+                                if(data.trivia.length <= themeIndex) {
+                                    message.channel.sendMessage("Incorrect index: Run `!trivia themes show` to get an index");
+                                    break;
+                                }
+                                
+                                message.channel.sendMessage("I am now removing **" + data.trivia[themeIndex].name + "** from the list of themes.");
+                                data.trivia.splice(themeIndex, 1);
                                 break;
                             
                             case "show":
                                 var themes = "";
                                 var themeIndex = -1;
                                 data.trivia.forEach(function(theme) {
-                                    themes += (themes.length > 0 ? "\n" : "") + "[" + ++themeIndex + "]" + "**" + theme.name + "**";
+                                    themes += (themes.length > 0 ? "\n" : "") + "`[" + ++themeIndex + "]`" + " **" + theme.name + "**";
                                 });
                                 
                                 message.channel.sendMessage(themes.length > 0 ? themes : "There are no theme entries yet.");
                                 break;
                             
                             default:
-                                message.channel.sendMessage("Incorrect parameters: `!trivia themes <add|remove|show> \"<name>\"`");
+                                message.channel.sendMessage("Incorrect parameters: `!trivia themes <add|remove|show>`");
+                                break;
+                        }
+                        break;
+                    
+                    case "questions":
+                        if(params.length < 2) {
+                            message.channel.sendMessage("Incorrect parameters: `!trivia questions <add|remove|show>`");
+                            break;
+                        }
+                        switch(params[1]) {
+                            case "add":
+                                break;
+                                
+                            case "remove":
+                                break;
+                                
+                            case "show":
+                                if(params[2] == null) {
+                                    message.channel.sendMessage("Incorrect parameters: `!trivia questions show \"<theme name>\" [+|-]` + to show answers.");
+                                    break;
+                                }
+                                
+                                var themeParam = params[2];
+                                data.trivia.forEach(function(theme) {
+                                    if(theme.name == themeParam) {
+                                        themeParam = theme;
+                                        return;
+                                    }
+                                });
+                                
+                                if(themeParam.name == null) {
+                                    message.channel.sendMessage("I can't find **" + params[2] + "**.");
+                                    break;
+                                }
+                                
+                                var questionsList = "";
+                                var questionIndex = -1;
+                                var showAnswers = params[3] == null ? false : params[3] == "+" ? true : false;
+                                themeParam.questions.forEach(function(q) {
+                                    questionsList += (questionsList.length > 0 ? "\n" : "") + "`[" + ++questionIndex + "]` **" + q.question + "**" + (showAnswers ? "\n" + getAnswers(q.answers) : "" );
+                                });
+                                
+                                message.channel.sendMessage(themeParam.questions.length > 0 ? questionsList : "There are no questions for **" + param[2] + "**.");
+                                break;
+                            
+                            default:
+                                message.channel.sendMessage("Incorrect parameters: `!trivia questions <add|remove|show>`");
                                 break;
                         }
                         break;
@@ -151,6 +214,7 @@ Answers: Answer here"`
                 `<> = required, [] = optional, | = or.`\n\
     \n\
     **Basic Commands**\n\
+    - !trivia `Shows a list of other commands to try out (questions, play, and answer still in development).`\n\
     - !test `Tests a new regex by printing all the parameters.`\n\
     - !hello `Sends a hello message back to the user.`\n\
     - !help `You goof.`\
@@ -172,6 +236,24 @@ Answers: Answer here"`
         }
     }
 });
+
+// Core Functions
+function addTrivia(triviaName) {
+    var triviaTemplate = { name: triviaName, questions: [] };
+    data.trivia.push(triviaTemplate);
+}
+
+function getAnswers(triviaQuestion) {
+    if(!(triviaQuestion.length > 0)) {
+        return "There are no answers for this question.";
+    }
+    var answersList = "";
+    var answerIndex = -1;
+    triviaQuestion.forEach(function(a) {
+        answersList += (answersList.length > 0 ? "\n" : "") + "    `[" + ++answerIndex + "]` " + a;
+    });
+    return answersList;
+}
 
 // Essential Functions
 
@@ -198,6 +280,18 @@ function checkForFile(fileName,callback)
     });
 }
 
+let PARAM_REGEX = /[^\s"']+|"([^"]*)"|'([^']*)'/g;
+
+function splitParams(command) {
+  let match = null;
+  let parts = [];
+
+  while (match = PARAM_REGEX.exec(command)){
+    parts.push(match[1] || match[2] || match[0]);
+  }
+  return parts;
+}
+
 
 
 
@@ -214,19 +308,6 @@ checkForFile('token.txt', function() {
         bot.login(token);
     });
 });
-
-
-let PARAM_REGEX = /[^\s"']+|"([^"]*)"|'([^']*)'/g;
-
-function split_params(command) {
-  let match = null;
-  let parts = [];
-
-  while (match = PARAM_REGEX.exec(command)){
-    parts.push(match[1] || match[2] || match[0]);
-  }
-  return parts;
-}
 
 /**
 
